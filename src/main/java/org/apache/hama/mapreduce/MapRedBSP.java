c49f09fa799a2cc0d06205c5169970d37ba2c0f0
Apurv Verma <dapurv5@gmail.com>
2016-07-18 00:48:35 -0700

    Step 1: fixing compile errors after bumping hama version and removing unused dependencies

--- src/main/java/org/apache/hama/mapreduce/MapRedBSP.java
+++ src/main/java/org/apache/hama/mapreduce/MapRedBSP.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hama.bsp.message.queue.MemoryQueue;
-import org.apache.hama.bsp.message.queue.SortedMessageQueue;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
